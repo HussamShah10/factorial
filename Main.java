6f5392ce4093207539a8240d10349ce0b2eb0dd2
HussamShah10 <145381883+HussamShah10@users.noreply.github.com>
2025-02-03 08:33:36 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        second();
+    }
+    
+        public static void second(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a number for its factorial: ");
+        
+        int num = scanner.nextInt();
+        
+        int answer = num;
+        for (int i = num -1; i > 0 ; i--){
+        answer *= i;
+        
+        }
+        System.out.println(answer); 
+        
+    }
+}
